0a7932619f6054ef1ab56327f6848f6ab0eb2ea4
natsunan <natsunan270@gmail.com>
2023-06-14 16:12:19 +0700

    first app

---
+++ resources/views/pages/link.blade.php
@@ -0,0 +1,11 @@
+
+<h1>
+    <span style='color:red;'>link</span>
+</h1>
+<hr />
+<hr>
+<a href="{{ route ('home')}}">Home</a>
+<a href="{{ route ('about')}}">About</a>
+<a href="{{ route ('contact')}}">Contact</a>
+<a href="{{ route ('post')}}">Post</a>
+<a href="{{ route ('link')}}">Link</a>
